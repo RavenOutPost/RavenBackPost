ce144b9fc4f3b3b811fa4cbfb16724af4f3cf806
Eliotools <eliot.martin@epitech.eu>
2024-12-07 16:54:05 +0100

    wip

--- src/server.js
+++ src/server.js
@@ -10,24 +10,55 @@ const db = nano.db.use('users');
 app.use(cors());
 app.get('/', (req, res) => res.status(200).json({message : 'Welcome to this API, documentation in comming'}))
 
-app.post('/addUser' ,async (req, res) => {
-  try {
-    console.log(req.body)
-    console.log(req.file)
-    const file = req.file;
-    const user = req.body;
-    
-    console.log(user)
-    if (!user) {
-        return res.status(200).json({message : 'no data'})
-        
-    }
-    const response = await db.insert(user);
-    res.status(201).json(response);
-  } catch (error) {
-    res.status(500).json({ message: 'Erreur lors de l\'ajout de l\'utilisateur', error });
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'uploads/'); 
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + '-' + file.originalname);
   }
 });
+
+const upload = multer({ storage: storage });
+
+
+app.post('/addUser' ,upload.fields([
+  { name: 'file', maxCount: 1 }, 
+  { name: 'name', maxCount: 1 }, 
+  { name: 'email', maxCount: 1 },
+]), (req, res) => {
+  try {
+    console.log('Champs textuels:', req.body.name, req.body.email);
+
+ 
+  console.log('Fichier téléchargé:', req.files.file);
+
+  res.status(200).json({
+    message: 'Fichier et données textuelles reçus avec succès',
+    file: req.files.file,   
+    name: req.body.name,   
+    email: req.body.email, 
+  })
+}
+catch(e) {
+  res.status(500).json({message : 'error', e})
+}
+});
+  //   console.log(req.body)
+  //   console.log(req.file)
+  //   const file = req.file;
+  //   const user = req.body;
+
+  //   console.log(user)
+  //   if (!user) {
+  //       return res.status(200).json({message : 'no data'})
+        
+  //   }
+  //   const response = await db.insert(user);
+  //   res.status(201).json(response);
+  // } catch (error) {
+  //   res.status(500).json({ message: 'Erreur lors de l\'ajout de l\'utilisateur', error });
+  // }
 
 app.get('/getUser/:id', async (req, res) => {
   try {
